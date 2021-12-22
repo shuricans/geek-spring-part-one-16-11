65d5dd2ffec713e480e20adfd184e173a855fbf5
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-12-22 09:18:55 +0300

    not sure if this solution is correct, but may be...

--- lesson-02-spring-basics/src/main/java/ru/geekbrains/CartService.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/CartService.java
@@ -1,12 +1,65 @@
 package ru.geekbrains;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.geekbrains.persist.Product;
 import ru.geekbrains.persist.ProductRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CartService {
 
     @Autowired
     private ProductRepository productRepository;
 
-    // TODO
+    private final Map<Long, Long> products; // idProduct, amount
+
+    public CartService() {
+        this.products = new HashMap<>();
+    }
+
+    public boolean put(Long productId) {
+        final Product product = productRepository.findById(productId);
+        if (product != null) {
+            products.merge(productId, 1L, Long::sum);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean remove(Long productId) {
+        final Product productFromRepo = productRepository.findById(productId);
+        final Long productFromCart = products.get(productId);
+        if (productFromRepo != null && productFromCart != null) {
+            final Long amount = products.get(productId);
+            if (amount == 1) {
+                products.remove(productId);
+            } else {
+                products.put(productId, amount - 1);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return products.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "Your cart is empty :(";
+        }
+        final StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%4s | %10s | %5s | %2s%n","id", "name", "price", "amount"));
+        products.forEach((key, value) -> {
+            final Product p = productRepository.findById(key);
+            sb.append(String.format("%4s | %10s | %5s | %2s%n", key, p.getName(), p.getPrice(), value));
+        });
+        return sb.toString();
+    }
 }
--- lesson-02-spring-basics/src/main/java/ru/geekbrains/persist/Product.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/persist/Product.java
@@ -6,12 +6,15 @@ public class Product {
 
     private String name;
 
+    private Integer price;
+
     public Product() {
     }
 
-    public Product(Long id, String name) {
+    public Product(Long id, String name, Integer price) {
         this.id = id;
         this.name = name;
+        this.price = price;
     }
 
     public Long getId() {
@@ -28,5 +31,22 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
