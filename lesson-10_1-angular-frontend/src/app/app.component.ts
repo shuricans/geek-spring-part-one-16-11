2a53a52906ac1876ff69b7d6a7c55acdc218a354
Sasha Karachev <shuricans@gmail.com>
2022-01-28 10:08:26 +0300

    init angular

---
+++ lesson-10_1-angular-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'lesson-10_1-angular-frontend';
+}
