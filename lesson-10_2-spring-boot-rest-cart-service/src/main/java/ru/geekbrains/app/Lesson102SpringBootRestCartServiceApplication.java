2bb5a8b6fc2f772810e7ef05cb7e21a0b303b8f4
Sasha Karachev <shuricans@gmail.com>
2022-01-28 10:22:07 +0300

    init REST part of lesson 10

---
+++ lesson-10_2-spring-boot-rest-cart-service/src/main/java/ru/geekbrains/app/Lesson102SpringBootRestCartServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson102SpringBootRestCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson102SpringBootRestCartServiceApplication.class, args);
+    }
+
+}
---
+++ lesson-10_2-spring-boot-rest-cart-service/src/test/java/ru/geekbrains/app/lesson10_2springbootrestcartservice/Lesson102SpringBootRestCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.app.lesson10_2springbootrestcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson102SpringBootRestCartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
